769a7c722ec5d3037a33c2fdf297e6c441f35b79
babji <2300039117@kluniversity.in>
2025-07-28 10:25:53 +0530

    added p line in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
+      <p>CI/CD  cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+
     </div>
   );
 }
